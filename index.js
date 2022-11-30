d2dcd9946be965234641dc2225064bd1a7800c47
abdihamid <ibrahimabdy58@gmail.com>
2022-11-30 09:46:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, Sammy){
+    
+    return drivers.filter(function (driver){
+        return driver.toLowerCase() === Sammy.toLowerCase()
+    })
+};
+
+function fuzzyMatch(drivers, Sammy){
+    return drivers.filter(function (driver){
+        return driver.toLowerCase().substring(0, Sammy.length) === Sammy.toLowerCase()
+    })
+};
+function matchName(drivers,Sally){
+    return drivers.filter(function (driver) {
+        return driver.name === Sally
+    })
+}
